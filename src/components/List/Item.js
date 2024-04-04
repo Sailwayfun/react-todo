d7146d18f566dec973bd959d81107653de328b8a
Sailwayfun <liaoleon000513@gmail.com>
2024-04-04 11:06:11 +0800

    add trapezoid left border to list iotem

--- src/components/List/Item.js
+++ src/components/List/Item.js
@@ -1,11 +1,18 @@
 import DeleteBtn from "./DeleteBtn";
 
+const trapezoidClasses = "h-14 w-0 border-r-4 border-r-blue-500 border-y-4 border-y-transparent border-solid";//左邊的梯形
+
 const Item = ({ todo }) => {
+
     return (
-        <li className="p-2 border-b bg-white text-blue-500 flex items-center justify-between">
-            <p>{ todo }</p>
-            <DeleteBtn />
-        </li>
+        <div className="flex">
+            <div className={ trapezoidClasses } />
+            <li className="flex-1 p-2 border-b bg-white text-blue-500 flex items-center justify-between">
+                <p>{ todo }</p>
+                <DeleteBtn />
+            </li>
+        </div>
+
     );
 };
 
