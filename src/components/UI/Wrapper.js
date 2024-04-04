24402d02010f24aaf99521d8847638a023aa9ed9
Sailwayfun <liaoleon000513@gmail.com>
2024-04-04 18:43:47 +0800

    uncenter the app

--- src/components/UI/Wrapper.js
+++ src/components/UI/Wrapper.js
@@ -1,6 +1,6 @@
 const Wrapper = ({ children }) => {
     return (
-        <div className="w-[768px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 aspect-square flex flex-col justify-center p-10 bg-gradient-to-b from-blue-100 to-purple-100 overflow-y-scroll fixed">
+        <div className="w-full p-10 flex flex-col justify-center bg-gradient-to-b from-blue-100 to-purple-100 overflow-y-scroll" >
             { children }
         </div>
     );
