82f9712dac42cbce2a46ccd83017d7d096d2b716
Sailwayfun <liaoleon000513@gmail.com>
2024-04-04 16:22:30 +0800

    center whole App

--- src/components/UI/Wrapper.js
+++ src/components/UI/Wrapper.js
@@ -1,6 +1,6 @@
 const Wrapper = ({ children }) => {
     return (
-        <div className="max-w-3xl aspect-square flex flex-col justify-center p-10 bg-gradient-to-b from-blue-100 to-purple-100 overflow-scroll">
+        <div className="w-[768px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 aspect-square flex flex-col justify-center p-10 bg-gradient-to-b from-blue-100 to-purple-100 overflow-y-scroll fixed">
             { children }
         </div>
     );
