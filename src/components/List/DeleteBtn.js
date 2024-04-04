49f2be5895393c7113bb684bca13ebde02ab5b08
Sailwayfun <liaoleon000513@gmail.com>
2024-04-04 21:51:00 +0800

    style: switch deleteIcon to heroicon

--- src/components/List/DeleteBtn.js
+++ src/components/List/DeleteBtn.js
@@ -1,6 +1,10 @@
+import DeleteIcon from "../UI/DeleteIcon";
+
 const DeleteBtn = ({ onDelete }) => {
     return (
-        <button className="cursor-pointer p-2 ml-auto" onClick={ onDelete }>X</button>
+        <button className="cursor-pointer p-2 ml-auto" onClick={ onDelete }>
+            <DeleteIcon />
+        </button>
     );
 };
 
