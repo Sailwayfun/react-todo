7d25cfb3705dc23f9d9156d531a3ca36f645b3bb
Sailwayfun <liaoleon000513@gmail.com>
2024-04-05 11:23:40 +0800

    fix: add back icon in search input

--- src/components/Search/Button.js
+++ src/components/Search/Button.js
@@ -1,12 +1,14 @@
 import SearchIcon from "../UI/SearchIcon";
+import BackIcon from "../UI/BackIcon";
 
-const Button = ({ onSearch }) => {
+const Button = ({ onSearch, isSearching }) => {
     return (
         <button
             onClick={ onSearch }
-            className="w-6 h-6 absolute top-1 right-1"
+            className="w-12 h-6 absolute top-1 right-1 flex gap-3"
         >
             <SearchIcon />
+            { isSearching ? <BackIcon /> : null }
         </button>
     );
 };
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,15 @@
 import Button from "./Button";
+import { useState } from "react";
 
 const Search = ({ search, setSearch, onSearch }) => {
+
+    const [isSearching, setIsSearching] = useState(false);
+
+    const searchActions = {
+        focus: () => setIsSearching(true),
+        blur: () => setIsSearching(false),
+    };
+
     return (
         <div className="flex justify-end relative">
             <input
@@ -8,8 +17,10 @@ const Search = ({ search, setSearch, onSearch }) => {
                 value={ search }
                 onChange={ (e) => setSearch(e.target.value) }
                 className="w-40 h-8 px-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:w-2/3"
+                onBlur={ searchActions.blur }
+                onFocus={ searchActions.focus }
             />
-            <Button onSearch={ onSearch } />
+            <Button onSearch={ onSearch } isSearching={ isSearching } />
         </div>
 
     );
