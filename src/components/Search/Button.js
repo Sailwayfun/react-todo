59dfd2a85a257f0c7a7f4ff2e281fd1eb22377cc
Sailwayfun <liaoleon000513@gmail.com>
2024-04-05 18:40:44 +0800

    fomat

--- src/components/Search/Button.js
+++ src/components/Search/Button.js
@@ -4,9 +4,7 @@ import BackButton from "../UI/BackIcon";
 const Button = ({ onSearch, isSearching, onBackToAllTodos }) => {
     return (
         <div className="flex gap-3 absolute h-6 top-1 right-5">
-            <button
-                onClick={ onSearch }
-                className="w-full h-full"
+            <button onClick={ onSearch } className="w-full h-full"
             >
                 <SearchIcon />
             </button>
