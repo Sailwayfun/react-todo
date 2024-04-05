4a3142dc6a0daf45263dad5d647ff1ae6ce6ed18
Sailwayfun <liaoleon000513@gmail.com>
2024-04-05 11:01:08 +0800

    style: search input expands on focus

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,7 +7,7 @@ const Search = ({ search, setSearch, onSearch }) => {
                 type="text"
                 value={ search }
                 onChange={ (e) => setSearch(e.target.value) }
-                className="w-40 h-8 px-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                className="w-40 h-8 px-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:w-2/3"
             />
             <Button onSearch={ onSearch } />
         </div>
