11057340bc3fe049ac3ecf1f3732f5d11efcf6dc
Sailwayfun <liaoleon000513@gmail.com>
2024-04-05 10:56:54 +0800

    style: change add-to-list button text to heroicon

--- src/components/NewTodo/index.js
+++ src/components/NewTodo/index.js
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import AddIcon from "../UI/AddIcon";
 
 const NewTodo = ({ onAddTodo }) => {
 
@@ -19,7 +20,9 @@ const NewTodo = ({ onAddTodo }) => {
             <label htmlFor="newTodo" className="text-gray-500 py-2">Add to list</label>
             <div className="flex gap-2 items-center">
                 <input id="newTodo" type="text" className="flex-1 px-2 py-1" ref={ inputRef } />
-                <button className="bg-blue-500 w-6 h-6 p-4 flex justify-center items-center rounded-sm text-lg" onClick={ () => onAddTodo(getNewTodo()) }>+</button>
+                <button className="bg-blue-500 w-6 h-6 p-4 flex justify-center items-center rounded-sm text-lg" onClick={ () => onAddTodo(getNewTodo()) }>
+                    <AddIcon />
+                </button>
             </div>
         </div>
     );
