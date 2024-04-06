83f2eb9ad928d5c436637cb7366bef1857bcf4a8
Sailwayfun <liaoleon000513@gmail.com>
2024-04-06 12:16:07 +0800

    fix: search input blur conflict with backbtn click

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import { useState, forwardRef } from "react";
+import { useState, forwardRef, useEffect } from "react";
 
 const _Search = ({ search, setSearch, onSearch, onBackToAllTodos, onCompositionStart, onCompositionEnd }, ref) => {
 
@@ -9,6 +9,17 @@ const _Search = ({ search, setSearch, onSearch, onBackToAllTodos, onCompositionS
         setIsSearching(true);
     }
     //Search component在focus的時候，會顯示SearchIcon和BackIcon，這個功能是透過focusSearchInput來控制的
+
+    useEffect(() => {
+        function onMouseDown(e) {
+            if (ref.current && !ref.current.contains(e.target)) {
+                setIsSearching(false);
+            }
+        }
+        document.addEventListener("mousedown", onMouseDown);
+        return () => document.removeEventListener("mousedown", onMouseDown);
+    });
+    //之所以不用input的onBlur，是為了避免input的onBlur和button的onClick事件衝突，所以這裡使用document的mousedown事件來偵測點擊事件
 
     return (
         <div className="flex justify-end relative">
--- src/components/UI/BackIcon.js
+++ src/components/UI/BackIcon.js
@@ -1,8 +1,8 @@
 const BackButton = ({ backToAllTodos }) => {
     return (
         <button onClick={ backToAllTodos } className="contents">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-full h-full" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } className="w-full h-full stroke-current">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
         </button>
     );
